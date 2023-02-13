e7c4e35c781ca9608c4b79e4642c3a18d4d91d78
320160642 <mefimova62@gmail.com>
2023-02-13 18:41:11 +0300

    debounce & pagination

--- src/components/Pizzablock/Search/Search.jsx
+++ src/components/Pizzablock/Search/Search.jsx
@@ -1,9 +1,34 @@
-import { useContext } from 'react';
+import { useContext, useRef, useState, useCallback } from 'react';
 import { SearchContext } from '../../../App';
 import styles from './Search.module.scss';
+import debounce from 'lodash.debounce';
+
 
 function Search() {
-    const { searchValue, setSearchValue } = useContext(SearchContext);
+    const [value, setValue] = useState('');
+    const { setSearchValue } = useContext(SearchContext);
+    const inputRef = useRef();
+
+    const onClickClear = () => {
+        setSearchValue('');
+        setValue('');
+        // вытащить ссылку на DOM-элемент
+        // document.querySelector('input').focus();
+        inputRef.current.focus();
+    }
+
+    // useCallback сохранить ссылку на функцию
+    const updateSearchValue = useCallback(
+        debounce((str) => {
+            setSearchValue(str);
+        }, 250),
+        [],
+    )
+
+    const onChangeInput = (e) => {
+        setValue(e.target.value);
+        updateSearchValue(e.target.value);
+    }
 
     return (
         <div
@@ -46,14 +71,15 @@ function Search() {
                 </line>
             </svg>
             <input
-                onChange={(e) => setSearchValue(e.target.value)}
+                ref={inputRef}
+                onChange={onChangeInput}
                 className={styles.input}
                 placeholder="Поиск пиццы..."
-                value={searchValue}
+                value={value}
             />
-            {searchValue && (
+            {value && (
                 <svg
-                    onClick={() => setSearchValue('')}
+                    onClick={onClickClear}
                     className={styles.clear}
                     xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 64 64" fill="none">
                     <path d="M22.6066 21.3934C22.2161 21.0029 21.5829 21.0029 21.1924 21.3934C20.8019 21.7839 20.8019 22.4171 21.1924 22.8076L22.6066 21.3934ZM40.9914 42.6066C41.3819 42.9971 42.0151 42.9971 42.4056 42.6066C42.7961 42.2161 42.7961 41.5829 42.4056 41.1924L40.9914 42.6066ZM21.1924 41.1924C20.8019 41.5829 20.8019 42.2161 21.1924 42.6066C21.5829 42.9971 22.2161 42.9971 22.6066 42.6066L21.1924 41.1924ZM42.4056 22.8076C42.7961 22.4171 42.7961 21.7839 42.4056 21.3934C42.0151 21.0029 41.3819 21.0029 40.9914 21.3934L42.4056 22.8076ZM21.1924 22.8076L40.9914 42.6066L42.4056 41.1924L22.6066 21.3934L21.1924 22.8076ZM22.6066 42.6066L42.4056 22.8076L40.9914 21.3934L21.1924 41.1924L22.6066 42.6066Z" fill="black" />
